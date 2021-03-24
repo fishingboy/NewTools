44847f1e4104148f1f69c3829595105ad142cc48
Leo Kuo <leokuo@qnap.com>
2021-03-24 17:12:06 +0800

    把 i18n 工具改好 --  php7.3 artisan i18n:deploy 20210324-SW-QVR-AI-Pack-i18n.csv

--- app/Console/Commands/I18n.php
+++ app/Console/Commands/I18n.php
@@ -12,7 +12,7 @@ class I18n extends Command
      *
      * @var string
      */
-    protected $signature = 'i18n';
+    protected $signature = 'i18n:deploy {csv_file?} {--preview}';
 
     /**
      * The console command description.
@@ -38,18 +38,42 @@ class I18n extends Command
      */
     public function handle()
     {
-        // @todo: csv 檔名改為傳入
         // @todo: 已存在的 key 只做更新
         // @todo: 處理雙引號問題
         // @todo: 畫面加一些提示
 
-        $csv_file = "20210316-QuWan-i18n.csv";
+        $csv_file = $this->argument('csv_file') ?? "20210316-QuWan-i18n.csv";
+        $preview = $this->option('preview');
+
+        // 顯示執行提示
+        $this->show_command_info($csv_file, $preview);
+
+        // 取得 i18n 資料
         $response = I18nService::getData($csv_file);
         $i18n = $response['i18n'];
 
         $env = "sw";
-        $response = I18nService::writeFiles($i18n, $env);
+        if ($preview) {
+            // 預覽
+            echo "<pre>i18n = " . print_r($i18n, true) . "</pre>\n";
+        } else {
+            // 寫入
+            $response = I18nService::writeFiles($i18n, $env);
+            echo "匯入 i18n 完畢。\n";
+        }
+    }
 
-        echo "匯入 i18n 完畢。\n";
+    /**
+     * 顯示執行命令的提示
+     * @param $csv_file
+     * @param $preview
+     */
+    public function show_command_info($csv_file, $preview)
+    {
+        echo "==========  i18n 佈屬工具  ==========\n";
+        echo "\n";
+        echo "[csv_file]: {$csv_file}\n";
+        echo "[preview]: {$preview}\n";
+        echo "\n";
     }
 }
